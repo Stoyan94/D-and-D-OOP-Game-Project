d1bbc5a08414130064ff6d0bd8f22cac31a0126e
Stoyan <podlobg@gmail.com>
2025-11-01 19:55:03 +0200

    Initial Project commit

---
+++ DandD-OOP/DandD-OOP/HeroAbsClass/Hero.cs
@@ -0,0 +1,51 @@
+﻿namespace DandD_OOP.HeroAbsClass
+{
+    public class Hero
+    {
+        private const int minLevel = 1;
+        protected virtual int maxLevel => 100;
+
+        private const int MinNameLength = 2;
+
+        private string name;
+        private int level = 1;
+
+        public Hero(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < MinNameLength)
+                {
+                    throw new ArgumentException(string.Format(HeroErrorMessages.InvalidNameError, MinNameLength));
+                }
+                name = value;
+            }
+        }
+
+        public int Level
+        {
+            get => level;
+            set
+            {
+                if (value < minLevel)
+                {
+                    throw new ArgumentException();
+                }
+
+                if (value > maxLevel)
+                {
+                    throw new ArgumentException($"Level cannot exceed {maxLevel}.");
+                }
+
+                level = value;
+            }
+        }
+
+    }
+}
---
+++ DandD-OOP/DandD-OOP/HeroErrorMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace DandD_OOP
+{
+    static class HeroErrorMessages
+    {
+        public const string InvalidNameError = 
+            "Hero name cannot be empty and must contain at least {0} letters.";
+    }
+}
